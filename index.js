dbd2e18b5f171b7eecf50faca06b4c629e7e66db
Emmanuel T <emmanuel.then@outlook.com>
2023-02-23 21:44:15 -0500

    cleaned up some code, added darkmode/lightmode icons and functionality with JS, added login and signup html page

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const subs = ['bronze', 'silver', 'gold']
-
-subs.forEach(index => console.log(`Congrats you are a ${index} member`))
+/* Toggle for darkmode */
+var icon = document.getElementById('icon');
+icon.onclick = function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "resources/lightmode.png";
+    } else {
+        icon.src = "resources/darkmode.png";
+    }
+}
 
